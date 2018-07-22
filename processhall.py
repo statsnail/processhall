d56e4d590a6b7be1528b359f32c73fe9f8d17e36
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2018-07-22 19:22:44 +0200

    added correct modules

--- processhall.py
+++ processhall.py
@@ -1,9 +1,11 @@
 ##!/usr/bin/env python
 
-# Hello there
+import scale.scale
+import labelwriter.labelwriter
 
 def main():
     print("running processhall application")
+    myscale = Scale('192.168.1.4','4001')
 
 if __name__ == '__main__':
     main()
