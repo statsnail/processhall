16511260d5f4990bab20e6bc59462ed6dec01278
Joakim Skjefstad <skjefstad.joakim@gmail.com>
2018-07-22 18:54:22 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# processhall
+
+GUI for managing labels and scales in process hall of Statsnail AS
+
+Software package depends on
+py-scale
+py-labelwriter
---
+++ processhall.py
@@ -0,0 +1,9 @@
+##!/usr/bin/env python
+
+# Hello there
+
+def main():
+    print("running processhall application")
+
+if __name__ == '__main__':
+    main()
